a861fd2e2d9d54c1e66b3788d1e4fd9e77931626
Mike Jin <mikejin@outlook..com>
2025-04-21 13:11:24 -0400

    updated models

--- db/models.py
+++ db/models.py
@@ -3,10 +3,30 @@ import datetime
 
 
 # Create your models here.
-class Building(models.Model):
-    address = models.CharField(max_length=100)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Service(models.Model):
+    name = models.CharField(max_length=20)
     def __str__(self):
         return self.name
+
+
+class Building(models.Model):
+    address = models.CharField(max_length=100)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.address
+
+
+
 
 
 
