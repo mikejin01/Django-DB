0098b4a6eca1c2d25c5a4ffdedee7e580848c576
Mike Jin <mikejin@outlook..com>
2025-04-21 10:46:42 -0400

    Added models

--- db/models.py
+++ db/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
+class Building(models.Model):
+    address = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+
